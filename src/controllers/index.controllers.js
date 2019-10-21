37630c99abaa33aef23097096735c6efbd9bf106
TheDarkWolf <franciscopugh01@gmail.com>
2019-10-21 14:23:47 -0300

    CRUD finished

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -10,8 +10,48 @@ const pool = new Pool({
 const getUsers = async (req, res) => {
     const response = await pool.query('SELECT * FROM users ORDER BY id ASC');
     res.status(200).json(response.rows);
+};
+
+const getUserById = async (req,res) => {
+    const response = await pool.query('SELECT * FROM users WHERE id = $1', [req.params.id]);
+    res.status(500).json(response.rows);
 }
 
+const createUser = async (req,res) => {
+    const {name, email} = req.body;
+    
+    const response = await pool.query('INSERT INTO users(name, email) VALUES ($1, $2)', [name, email]);
+    console.log(response);
+    res.json({
+        message: 'User added',
+        body: {
+            user: {name, email}
+        }
+    })
+};
+
+const updateUser = async (req,res) => {
+    const {name, email} = req.body;
+    
+    const response = await pool.query('UPDATE users SET name = $1, email = $2 WHERE id = $3', [
+        name,
+        email,
+        req.params.id
+    ]);
+    console.log(response);
+    res.json('User updated');
+};
+
+const deleteUser = async (req,res) => {
+    const response = await pool.query('DELETE FROM users WHERE id= $1', [req.params.id]);
+    console.log(response);
+    res.json(`User ${req.params.id} deleted`);
+};
+
 module.exports = {
-    getUsers
+    getUsers, 
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser 
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,11 @@
 const { Router } = require('express');
 const router = Router();
-const { getUsers } = require('../controllers/index.controllers');
+const { getUsers, getUserById, createUser, updateUser, deleteUser} = require('../controllers/index.controllers');
 
-router.get('/users',getUsers );
+router.get('/users', getUsers );
+router.get('/users/:id',getUserById);
+router.post('/users', createUser);
+router.put('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
 
 module.exports = router;
